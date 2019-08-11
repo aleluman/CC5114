582803818dfe3dac94b7526ec6ec043a3386ce42
aleluman <aleluman@gmail.com>
2019-08-11 19:14:37 -0400

    Fixed errorplot

--- Exercises/points.py
+++ Exercises/points.py
@@ -15,7 +15,6 @@ class Points:
     def generate_points(self):
         a = random.randint(-10, 10)
         b = random.randint(-10, 10)
-        print(a, b)
         for i in range(self.n_points):
             p = [random.random() * 20 - 10, random.random() * 20 - 10]
             self.points.append(np.array(p))
@@ -44,21 +43,20 @@ def plot_points(n):
     plt.show()
 
 
-def plot_error():
-    points = Points(2500, 45)
+def plot_error(k):
+    points = Points(k, 45)
     errors = []
-    for n in range(2500):
+    for n in range(k):
         perceptron = Perceptron(random.random() * 2 - 1, np.array([random.random() * 2 - 1, random.random() * 2 - 1]))
         for i in range(n):
             perceptron.learn(points.points[i], 0.1, points.outputs[i])
         real_outputs = [perceptron.output(x) for x in points.points[:1000]]
         errors.append(
             1 - sum([abs(i - j) for i, j in zip(points.outputs.tolist()[:1000], real_outputs)]) / len(real_outputs))
-        print(n)
-    plt.plot(range(2500), errors)
+    plt.plot(range(k), errors)
     plt.ylim(0, 1)
     plt.show()
 
 
 if __name__ == '__main__':
-    plot_error()
+    plot_error(1000)
