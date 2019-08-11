c8da772ee98272915937c0756dbee7b4e6d64bd2
aleluman <aleluman@gmail.com>
2019-08-11 19:11:34 -0400

    Fixed errorplot

---
+++ Exercises/perceptron.py
@@ -0,0 +1,55 @@
+import unittest
+import numpy as np
+
+
+class Perceptron:
+    def __init__(self, bias, weights):
+        self.bias = bias
+        self.weights = weights
+        self.result = 0
+        self.real_output = 0
+
+    def output(self, inputs):
+        assert self.weights.size == inputs.size, "Mismatch between inputs and weights size."
+
+        self.result = np.sum(self.weights * inputs)
+        if self.result + self.bias > 0:
+            return 1
+        else:
+            return 0
+
+    v_output = np.vectorize(output)
+
+    def learn(self, inputs, learning_rate, desired_output):
+        diff = desired_output - self.output(inputs)
+        self.weights = self.weights + (learning_rate * inputs * diff)
+        self.bias += (learning_rate * diff)
+
+
+class TestPerceptron(unittest.TestCase):
+    def setUp(self):
+        self.nand_perceptron = Perceptron(1.5, np.array([-1, -1]))
+        self.and_perceptron = Perceptron(-1.5, np.array([1, 1]))
+        self.or_perceptron = Perceptron(-0.5, np.array([1, 1]))
+
+    def test_nand(self):
+        self.assertEqual(self.nand_perceptron.output(np.array([1, 1])), 0)
+        self.assertEqual(self.nand_perceptron.output(np.array([0, 1])), 1)
+        self.assertEqual(self.nand_perceptron.output(np.array([1, 0])), 1)
+        self.assertEqual(self.nand_perceptron.output(np.array([0, 0])), 1)
+
+    def test_and(self):
+        self.assertEqual(self.and_perceptron.output(np.array([1, 1])), 1)
+        self.assertEqual(self.and_perceptron.output(np.array([0, 1])), 0)
+        self.assertEqual(self.and_perceptron.output(np.array([1, 0])), 0)
+        self.assertEqual(self.and_perceptron.output(np.array([0, 0])), 0)
+
+    def test_or(self):
+        self.assertEqual(self.or_perceptron.output(np.array([1, 1])), 1)
+        self.assertEqual(self.or_perceptron.output(np.array([0, 1])), 1)
+        self.assertEqual(self.or_perceptron.output(np.array([1, 0])), 1)
+        self.assertEqual(self.or_perceptron.output(np.array([0, 0])), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Exercises/points.py
@@ -0,0 +1,64 @@
+import random
+from perceptron import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Points:
+    def __init__(self, n_points, seed):
+        random.seed(seed)
+        self.n_points = n_points
+        self.points = []
+        self.outputs = np.zeros(n_points)
+        self.a, self.b = self.generate_points()
+
+    def generate_points(self):
+        a = random.randint(-10, 10)
+        b = random.randint(-10, 10)
+        print(a, b)
+        for i in range(self.n_points):
+            p = [random.random() * 20 - 10, random.random() * 20 - 10]
+            self.points.append(np.array(p))
+            if p[1] > a * p[0] + b:
+                self.outputs[i] = 1
+        return a, b
+
+
+def plot_points(n):
+    points = Points(10000, 54)
+    perceptron = Perceptron(random.random() * 2 - 1, np.array([random.random() * 2 - 1, random.random() * 2 - 1]))
+    for i in range(n):
+        perceptron.learn(points.points[i], 0.1, points.outputs[i])
+
+    real_outputs = [perceptron.output(x) for x in points.points]
+    x_points = [x[0] for x in points.points]
+    y_points = [x[1] for x in points.points]
+    for i in range(1000):
+        if real_outputs[i] == 0:
+            plt.scatter(x_points[i], y_points[i], c='r')
+        else:
+            plt.scatter(x_points[i], y_points[i], c='b')
+    plt.plot(np.arange(-10, 10.1, 0.1), np.arange(-10, 10.1, 0.1) * points.a + points.b)
+    plt.xlim(-10, 10)
+    plt.ylim(-10, 10)
+    plt.show()
+
+
+def plot_error():
+    points = Points(2500, 45)
+    errors = []
+    for n in range(2500):
+        perceptron = Perceptron(random.random() * 2 - 1, np.array([random.random() * 2 - 1, random.random() * 2 - 1]))
+        for i in range(n):
+            perceptron.learn(points.points[i], 0.1, points.outputs[i])
+        real_outputs = [perceptron.output(x) for x in points.points[:1000]]
+        errors.append(
+            1 - sum([abs(i - j) for i, j in zip(points.outputs.tolist()[:1000], real_outputs)]) / len(real_outputs))
+        print(n)
+    plt.plot(range(2500), errors)
+    plt.ylim(0, 1)
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_error()
